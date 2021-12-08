e08a2670993350a840eced5a7d8218400c157dcb
hgusi <hgusinac@gmail.com>
2021-12-08 09:12:56 +0100

    Initial commit

---
+++ src/main/java/com/example/recipe_jpa/RecipeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipe_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeJpaApplication.class, args);
+    }
+
+}
